46b3fdf4558767140022838afe998fdd8591146b
RDPodcasting <56129760+RDPodcasting@users.noreply.github.com>
2019-10-08 19:14:14 -0300

    Criando Tela SOBRE.
    Crianção da tela.

--- RdPodcastingWeb/Controllers/HomeController.cs
+++ RdPodcastingWeb/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace RdPodcastingWeb.Controllers
             return View();
         }
 
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
