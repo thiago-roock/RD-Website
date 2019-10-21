89898e89e612a6b701e8f19ba210ae9efd805524
RDPodcasting <56129760+RDPodcasting@users.noreply.github.com>
2019-10-21 16:00:23 -0300

    Pagina Episodios.

--- RdPodcastingWeb/Controllers/HomeController.cs
+++ RdPodcastingWeb/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace RdPodcastingWeb.Controllers
             return View();
         }
 
+        public ActionResult Episodios()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
