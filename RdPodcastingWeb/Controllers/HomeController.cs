7baccea037521264cc6567d2235218f754018fcb
Thiago de Melo Lima <thiago_badboy300@hotmail.com>
2019-12-24 09:48:28 -0300

    Commit de vespera de natal
    atualizações em todas páginas.

--- RdPodcastingWeb/Controllers/HomeController.cs
+++ RdPodcastingWeb/Controllers/HomeController.cs
@@ -6,27 +6,25 @@ using System.Web.Mvc;
 
 namespace RdPodcastingWeb.Controllers
 {
+    [OutputCache(CacheProfile = "CacheLong")]
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Sobre()
+         public ActionResult Sobre()
         {
             return View();
         }
-
         public ActionResult Episodios()
         {
             return View();
         }
-
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
--- RdPodcastingWeb/Scripts/contact.js
+++ RdPodcastingWeb/Scripts/contact.js
@@ -162,55 +162,5 @@ $(document).ready(function () {
         }
     }
 
-	/* 
-
-	7. Init Google Map
-
-	*/
-
-    function initGoogleMap() {
-        var myLatlng = new google.maps.LatLng(47.495962, 19.050966);
-        var mapOptions =
-        {
-            center: myLatlng,
-            zoom: 14,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            draggable: true,
-            scrollwheel: false,
-            zoomControl: true,
-            zoomControlOptions:
-            {
-                position: google.maps.ControlPosition.RIGHT_CENTER
-            },
-            mapTypeControl: false,
-            scaleControl: false,
-            streetViewControl: false,
-            rotateControl: false,
-            fullscreenControl: true,
-            styles:
-                [
-                    {
-                        "featureType": "road.highway",
-                        "elementType": "geometry.fill",
-                        "stylers": [
-                            {
-                                "color": "#ffeba1"
-                            }
-                        ]
-                    }
-                ]
-        }
-
-        // Initialize a map with options
-        map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-        // Re-center map after window resize
-        google.maps.event.addDomListener(window, 'resize', function () {
-            setTimeout(function () {
-                google.maps.event.trigger(map, "resize");
-                map.setCenter(myLatlng);
-            }, 1400);
-        });
-    }
-
+	
 });
